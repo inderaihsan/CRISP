0437d12e212ee1594ba3d187cdfc8fd167fe95de
Indera Ihsan <43025655+inderaihsan@users.noreply.github.com>
2022-08-28 04:27:23 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -88,24 +88,10 @@ fileupload()
 eda(dataframe) 
 
 st.header('Spatial Database Download') 
-if (uploaded) : 
-    download_necessary()
-if(downloaded & uploaded) : 
-    download_file()
+
 
 
 # Using object notation
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
-
-# Using "with" notation
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
 
 
 
