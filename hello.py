2dadbfd72525296f1f91de815fe226a0605407f0
Indera Ihsan <43025655+inderaihsan@users.noreply.github.com>
2022-08-29 00:36:29 +0700

    Update hello.py
    added remove missing function for categorical and numerical value with group by and aggregate function.

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
 from distutils.command.upload import upload
+from multiprocessing import dummy
+from turtle import onclick 
+import streamlit.components.v1 as components
 import streamlit as st 
 import pandas as pd
 import numpy as np 
@@ -8,7 +11,9 @@ import matplotlib.pyplot as plt
 import plotly.figure_factory as ff  
 import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report 
-import gdown
+import gdown 
+from dataprep.eda.missing import plot_missing 
+from dataprep.eda import create_report
 
 
 
@@ -27,7 +32,22 @@ def fileupload() :
         dataframe = pd.read_csv(uploaded_file)
         st.write(dataframe)  
         global uploaded 
-        uploaded = True
+        uploaded = True 
+        if 'dataset' not in st.session_state:
+                st.session_state['dataset'] = dataframe  
+        return dataframe
+
+def use_dummy_data() : 
+    global uploaded 
+    global dataframe
+    data = sns.load_dataset('penguins')  
+    dataframe = data 
+    uploaded = True 
+    if 'dataset' not in st.session_state:
+            st.session_state['dataset'] = dataframe  
+    st.write(dataframe) 
+    return dataframe
+    
 
 
 def convert(uploaded) :
@@ -65,30 +85,86 @@ def eda (dataframe) :
             pr = dataframe[options].profile_report()
             st_profile_report(pr) 
 
+ 
+def showmissing(dataframe) :   
+    if(uploaded) : 
+        missing_val = []
+        list_of_col = dataframe.columns.unique() 
+        for items in list_of_col : 
+            missing_val.append(dataframe[items].isna().sum()) 
+        
+        missing_tab = pd.DataFrame({'kolom' : list_of_col, 'number of missing' : missing_val}) 
+        st.bar_chart(data = missing_tab, x = 'kolom', y = 'number of missing') 
 
+    else : 
+        st.write("you haven't uploaded a file, please upload a CSV file or use seaborn dummy data") 
 
-def get_id(url) :  
-    res = url.split('/')
-    for el in res : 
-      if len(el)> 10 and el !='drive.google.com' and el!='https:' and el!='docs.google.com' and el!= 'spreadsheets': 
-        #print(el) 
-        return el 
+def fill_missing(dataframe) :  
+    sel_col = st.radio('please select a column to be filled', dataframe.columns) 
+    st.write(sel_col) 
 
+    #categorical missing value
+    if(type(st.session_state['dataset'][sel_col][0])==str) : 
+        sel_val = st.radio('select a value to replace the null : ', st.session_state['dataset'][st.session_state['dataset'][sel_col].notnull()][sel_col].unique()) 
+        if(st.button('fill null values')) : 
+            st.session_state['dataset'][sel_col] = st.session_state['dataset'][sel_col].apply(lambda x:sel_val if pd.isnull(x) else x) 
+            #showmissing(st.session_state['dataset'])  
+            st.session_state['dataset'] = st.session_state['dataset']  
 
+    #numerical missing value
+    if(type(st.session_state['dataset'][sel_col][0])!=str) : 
+        sel_method = st.radio('numerical method : ', ['Mean', 'Minimum', 'Maximum'], horizontal = True)  
+        sel_group = st.radio('select a group column' , st.session_state.dataset.select_dtypes(exclude='number').columns, horizontal=True) 
+        operator = {'Mean' : st.session_state.dataset.groupby(by=[sel_group]).mean().reset_index() , 
+                    'Minimum' : st.session_state.dataset.groupby(by=[sel_group]).min().reset_index(), 
+                    'Maximum' : st.session_state.dataset.groupby(by=[sel_group]).max().reset_index(), 
+                   } 
+        temp = operator[sel_method] 
+        gather = dict(zip(temp[sel_group], temp[sel_col]))
+        st.write(gather)
+        if(st.button('fill null values')) :  
+            index = 0
+            for x,y in zip(st.session_state.dataset[sel_group], st.session_state.dataset[sel_col]) : 
+                if (pd.isnull(y)) : 
+                    st.session_state.dataset[sel_col][index] = gather[x] 
+                    print('miss miss miss') 
+                    index=index+1 
+                index=index+1
+            st.session_state.dataset = st.session_state.dataset 
+            
+            
     
-
 ################################################algo#######################################################
+
+
 
 st.title('Welcome!') 
 st.write('this web app is a beta version of a completely GUI based exploratory data analysis.') 
 st.write('this app will guide you step by step through data analysis based on  [CRISP](https://en.wikipedia.org/wiki/Cross-industry_standard_process_for_data_mining) method. so the first step is to upload your file. ') 
 st.write('you can upload your file below using the form')
-fileupload()  
 
-eda(dataframe) 
+fileupload()   
 
+st.write('you can also use a dummy data from seaborn [penguins](https://github.com/mwaskom/seaborn-data/blob/master/penguins.csv) dataset ')
 
+dummyyy = st.checkbox('i want to use seaborn dataset') 
 
+if(dummyyy) : 
+    use_dummy_data() 
+    df = st.session_state['dataset']
+eda(dataframe)    
+
+st.header('Missing value') 
+
+if(uploaded) :  
+    tab_1, tab_2 = st.tabs(["Missing value", "Fill Missing Value"]) 
+    with tab_1 : 
+        df = st.session_state['dataset']
+        showmissing(df) 
+    with tab_2 : 
+        st.session_state['dataset']
+        fill_missing(df) 
+        showmissing(df)
 
 
 
